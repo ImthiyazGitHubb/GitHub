d82f8e660a4ae25501e401329e6d76388de6e51b
unknown <imthiyazuddeen15@gmail.com>
2024-04-23 15:36:18 +0530

    Added Code

--- src/org/gitproject/Sample_1.java
+++ src/org/gitproject/Sample_1.java
@@ -2,4 +2,21 @@ package org.gitproject;
 
 public class Sample_1 {
 
+	
+public static void main(String[] args) {
+	
+	
+	System.out.println("Suresh Completed his work");
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
 }
